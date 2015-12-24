5b47f6c739597a8d86e8cefeddf9bda22d5d0de9
13nu5h0n0k <elenapogrebetskaya@gmail.com>
2015-12-24 21:42:10 +0300

    Продвижение

--- lfs.c
+++ lfs.c
@@ -1,4 +1,38 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include "lfs.h"
+
+#define BLOCK_SIZE_DIVISOR 1024
+
+char *root;
+int lfs_fd;
+
+int lfs_create(const char *root, unsigned int block_size, unsigned long blocks_count)
+{
+	if (block_size % BLOCK_SIZE_DIVISOR != 0)
+		return -1;
+
+	unsigned long i;
+	char *filler;
+	struct lfs_superblock sb;
+
+	sb.rootdir_loc = 
+
+	lfs_fd = open(path, O_RDWR | O_CREAT | O_DIRECT);
+
+	
+
+	filler = calloc(block_size, sizeof(char));
+	lseek(lsf_fd, block_size, SEEK_SET);
+	for (i = 0; i < blocks_count; i++)		
+	    write(lfs_fd, filler, sizeof(filler));
+	
+}
+
+
+
 
 void lfs_refresh_ftable()
 {
--- lfs_ftable.h
+++ lfs_ftable.h
@@ -1,11 +1,19 @@
 #define LFS_FTABLE_CAPACITY 20
 
-struct lfs_ftable_entry {
+struct lfs_ftable_entry 
+{
     char name[255];
     unsigned long loc;
 };
 
-struct lfs_ftable {
+struct lfs_ftable 
+{
 	struct lfs_ftable_entry entries[LFS_FMAP_CAPACITY];
 	unsigned int size;	
 };
+
+void lfs_ftable_create(struct lfs_ftable *, unsigned int);
+
+int lfs_ftable_put(struct lfs_ftable *, const char *, unsigned long);
+
+unsigned long lfs_ftable_get(struct lfs_ftable *, const char *);
